f005840ec8a541f77f3f961ca40191d054524ea3
shuvojitNITW <shuvojitnitw@gmail.com>
2014-12-29 03:28:33 +0530

    testupload

---
+++ inifiles/existinguser.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<head>
+	login.
+</head>
+<body>
+<form action="<?=($_server())
+<?php
+	$servername="localhost";//172.30.122.71";
+	$username="root";
+	$password="bosco007";
+	$database="chat";
+	$conn=new mysqli($servername,$username,$password,$database);
+
+
